6655e020c371f2e9611338457f40d660152b4cd8
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-12-27 08:42:35 +0600

    HelloWorld problem solved

---
+++ src/JavaHelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * The {@code JavaHelloWorld} class demonstrates a simple Java program
+ * that prints "Hello World" to the console.
+ * @author <a href="https://github.com/sazid99246">Sheikh Md. Sazidul Islam</a>
+ * @version 1.0
+ * @since 27/12/2024
+ */
+public class JavaHelloWorld {
+
+    /**
+     * Prints "Hello World" to the console.
+     */
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * The main method is the entry point of the program.
+     *
+     * @param args command-line arguments (not used in this program)
+     */
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}
