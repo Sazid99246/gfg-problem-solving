438f82205338b4ba95fd95f3455055edf2fd11c3
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-01-14 07:51:31 +0600

    Eqailibrium problem solved

---
+++ src/Equilibrium.java
@@ -0,0 +1,23 @@
+public class Equilibrium {
+    public static int findEquilibrium(int[] arr) {
+        int rightSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            rightSum += arr[i];
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            rightSum -= arr[i]; // Update rightSum for the current element
+            if (leftSum == rightSum) {
+                return i; // Return the index where equilibrium is found
+            }
+            leftSum += arr[i]; // Update leftSum for the current element
+        }
+        return -1; // No equilibrium point found
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findEquilibrium(new int[]{1, 2, 0, 3}));
+        System.out.println(findEquilibrium(new int[]{1, 1, 1, 1}));
+    }
+}
