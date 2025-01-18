2b98ce846a745f485f3df1d587e1a4f9266bd694
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-01-18 07:25:31 +0600

    ReverseLinkedList problem solved

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,25 @@
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+public class ReverseLinkedList {
+    Node ReverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;  // Store next node
+            current.next = prev;  // Reverse current node's pointer
+            prev = current;       // Move pointers one step forward
+            current = next;
+        }
+
+        head = prev;  // Update head to new head (last non-null node)
+        return head;
+    }
+}
