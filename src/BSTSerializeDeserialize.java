b24d3f5a7d2a21a407ec1454aa55d703747ab862
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-02-16 08:59:32 +0600

    code reformatted

--- src/BSTSerializeDeserialize.java
+++ src/BSTSerializeDeserialize.java
@@ -4,20 +4,20 @@ import java.util.Queue;
 
 public class BSTSerializeDeserialize {
     public ArrayList<Integer> serialize(Node root) {
-        ArrayList<Integer> list=new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         list.add(root.data);
-        Queue<Node> queue=new LinkedList<>();
+        Queue<Node> queue = new LinkedList<>();
         queue.add(root);
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            while(size-->0){
-                Node cur=queue.poll();
-                Node left=cur.left;
-                Node right=cur.right;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                Node cur = queue.poll();
+                Node left = cur.left;
+                Node right = cur.right;
                 list.add(getVal(left));
                 list.add(getVal(right));
-                if(left!=null) queue.add(left);
-                if(right!=null) queue.add(right);
+                if (left != null) queue.add(left);
+                if (right != null) queue.add(right);
             }
         }
         return list;
@@ -25,33 +25,33 @@ public class BSTSerializeDeserialize {
 
     // Function to deserialize a list and construct the tree.
     public Node deSerialize(ArrayList<Integer> arr) {
-        Node root=new Node(arr.get(0));
-        int ind=1;
-        int n=arr.size();
-        Queue<Node> queue=new LinkedList<>();
+        Node root = new Node(arr.get(0));
+        int ind = 1;
+        int n = arr.size();
+        Queue<Node> queue = new LinkedList<>();
         queue.add(root);
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            while(size-->0){
-                Node cur=queue.poll();
-                Node left=getNode(ind++,arr);
-                Node right=getNode(ind++,arr);
-                cur.left=left;
-                cur.right=right;
-                if(left!=null) queue.add(left);
-                if(right!=null) queue.add(right);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                Node cur = queue.poll();
+                Node left = getNode(ind++, arr);
+                Node right = getNode(ind++, arr);
+                cur.left = left;
+                cur.right = right;
+                if (left != null) queue.add(left);
+                if (right != null) queue.add(right);
             }
         }
         return root;
     }
 
-    Node getNode(int ind,ArrayList<Integer> list){
-        if(ind>=list.size() || list.get(ind)==null) return null;
+    Node getNode(int ind, ArrayList<Integer> list) {
+        if (ind >= list.size() || list.get(ind) == null) return null;
         return new Node(list.get(ind));
     }
 
-    Integer getVal(Node cur){
-        if(cur==null) return null;
+    Integer getVal(Node cur) {
+        if (cur == null) return null;
         return cur.data;
     }
 }
