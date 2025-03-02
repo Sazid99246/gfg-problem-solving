56234d178c2c43fc54aecb0097841a23a848b542
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-03-02 22:41:53 +0600

    K Sized Subarray Maximum

---
+++ src/KSizedSubarrayMaximum.java
@@ -0,0 +1,22 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+public class KSizedSubarrayMaximum {
+    public ArrayList<Integer> maxOfSubarrays(int arr[], int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        int n = arr.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i =0; i < n; i++) {
+            while(dq.size() != 0 && arr[dq.getLast()] < arr[i]) {
+                dq.removeLast();
+            }
+            dq.addLast(i);
+            if(i - dq.getFirst() == k) dq.removeFirst();
+            if(i >= k -1) {
+                ans.add(arr[dq.getFirst()]);
+            }
+        }
+        return ans;
+    }
+}
